6aabc8e0c3897f65a5c5df5c9bdd2413980601e3
Yuki Chew <yukichew0803@gmail.com>
2023-03-24 13:39:54 +0800

    initial set up

--- JavaAssignment/src/javaassignment/admin/Admin.java
+++ JavaAssignment/src/javaassignment/admin/Admin.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package javaassignment.admin;
 
-/**
- *
- * @author yukichew
- */
 public class Admin {
     
 }
---
+++ JavaAssignment/src/javaassignment/user/UserLogin.java
@@ -0,0 +1,5 @@
+package javaassignment.user;
+
+public class UserLogin {
+    
+}
